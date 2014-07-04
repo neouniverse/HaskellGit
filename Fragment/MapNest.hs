cf3703032dc6f0c403e9b1fb57ceb7a7fbaf209d
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2014-07-04 15:57:25 +0900

    bug fix

--- Fragment/MapNest.hs
+++ Fragment/MapNest.hs
@@ -1,7 +1,7 @@
 {-******************************************
   *     File Name: MapNest.hs
   *        Author: neouniverse
-  * Last Modified: 2014/06/24 17:33:33
+  * Last Modified: 2014/07/04 15:55:56
   *******************************************-}
 
 mapWith :: (a -> b -> c) -> [a] -> [b] -> [[c]]
@@ -9,7 +9,7 @@ mapWith f [] _ = []
 mapWith f (x:xs) ys = map (f x) ys : mapWith f xs ys
 
 mapZipWith :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [[d]]
-mapZipWith f [] _ _ []
+mapZipWith f [] _ _ = []
 mapZipWith f (x:xs) ys zs = zipWith (f x) ys zs : mapZipWith f xs ys zs
 
 mapWith3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [[d]]
